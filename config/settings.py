31a40bd1c1f382f806951dfb0bad39341744197d
Safaa Hoz <SAFAHO@AC.SCE.AC.IL>
2026-05-23 22:58:17 +0300

    Fix Azure SQL connection params

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
@@ -93,7 +92,7 @@ DATABASES = {
 
         'OPTIONS': {
             'driver': 'ODBC Driver 18 for SQL Server',
-            'extra_params': 'Encrypt=yes;TrustServerCertificate=yes',
+            'extra_params': 'Encrypt=yes;TrustServerCertificate=yes;',
         },
     }
 }
